3099e34f4de43e8f0e9ebb4cf328ae1a4a92ecaf
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2020-02-26 17:19:53 +0530

    day#1 final

---
+++ csprojects/BankingSystem/BankAccountApp01/BankAccount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace BankAccountApp01
+{
+    internal class BankAccount
+    {
+        int accountNumber;
+        string name;
+        string password;
+        double balance;
+        double rate;
+
+        public BankAccount(int accountNumber, string name, string password, int balance, int rate)
+        {
+            this.accountNumber = accountNumber;
+            this.name = name;
+            this.password = password;
+            this.balance = balance;
+            this.rate = rate;
+        }
+
+        public void Withdraw(double amount, string password)
+        {
+            
+
+            if (amount <= 0)
+                Console.WriteLine("Amount should be positive");
+            else if (amount > balance)
+                Console.WriteLine("Insufficient balance");
+            else if (password != this.password)
+                Console.WriteLine("Invalid credentials");
+            else
+            {
+                Console.WriteLine("Please collect your cash");
+                balance -= amount;
+            }
+
+        }
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Account Number "+accountNumber);
+            Console.WriteLine("Name " + name);
+            Console.WriteLine("Balance " + balance);
+            Console.WriteLine("Password " + password);
+            Console.WriteLine("Rate " + rate);
+        }
+    }
+}
