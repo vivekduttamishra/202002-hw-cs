6e73e22fe9c4f1d580a6059c5fa46c8be1a6ce18
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2020-02-26 12:17:22 +0530

    day#1 update#2

--- csprojects/GeometrySystem/ConceptArchitect.Geometry/Triangle.cs
+++ csprojects/GeometrySystem/ConceptArchitect.Geometry/Triangle.cs
@@ -10,11 +10,18 @@ namespace ConceptArchitect.Geometry
     {
        public int s1, s2, s3; //has 3 sides called s1,s2,s3
 
+        //TODO: 1. CREATE A SET FUNCTION THAT TAKES
+        // X,Y,Z
+        //ASSIGNS TO S1,S2,S3 ONLY IF X,Y,Z ARE VALID SIDES OF TRIANGLE
+
+
+        //SHOULD RETURN -1 IF SIDES ARE NOT VALID
         public int Perimeter()
         {
             return s1 + s2 + s3;
         }
 
+        //SHOULD PRINT INVALID TRIANGLE IF SIDES ARE NOT VALID
         public void Draw()
         {
             Console.WriteLine("Triangle<{0},{1},{2}> drawn",s1,s2,s3);
