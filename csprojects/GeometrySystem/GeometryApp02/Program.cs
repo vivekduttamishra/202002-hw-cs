d612dd5eb78ff613719f39d4b64c9e2efb1b1f6f
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2020-02-26 14:06:29 +0530

    day#1 update#5

--- csprojects/GeometrySystem/GeometryApp02/Program.cs
+++ csprojects/GeometrySystem/GeometryApp02/Program.cs
@@ -11,18 +11,31 @@ namespace GeometryApp02
     {
         static void Main(string[] args)
         {
-            var t1 = new Triangle();
-            t1.Set(3, 4, 5);
+            //when is Triangle t1 created?
+            //here?
+            var t1 = new Triangle(3,4,5);
+            //or here?
+            //t1.Set(3, 4, 5);
             TestTriangle(t1);
 
-            var t2 = new Triangle();
+            var t2 = new Triangle(3,6,12);
             t2.Set(3, 6, 12);
             TestTriangle(t2); //should print error message for invalid triangle
+
+
+            Triangle t3 = new Triangle();
+            //t3.Set() not called.
+            TestTriangle(t3);
+
+            var t4 = new Triangle(5);
+            TestTriangle(t4);
+
         }
         static  void TestTriangle(Triangle t)
         {
             t.Draw();
             Console.WriteLine("Perimeter is :"+t.Perimeter());
+            Console.WriteLine();
         }
     }
 }
--- csprojects/GeometrySystem/GeometryApp02/Triangle.cs
+++ csprojects/GeometrySystem/GeometryApp02/Triangle.cs
@@ -9,22 +9,65 @@ namespace ConceptArchitect.Geometry
     public class Triangle
     {
         int s1, s2, s3; //has 3 sides called s1,s2,s3
+        
+        //replaces default constructor!
+        public Triangle(int x,int y, int z)
+        {
+            Set(x, y, z);
+        }
+
+        //can have a second constructor
+        public Triangle(int side)
+        {
+            Set(side, side, side);
+        }
+
+        public Triangle()
+        {
+            Set(1, 1, 1);
+        }
+
+        public void Set(int x, int y, int z)
+        {
+            if(x>0 && y>0 && z>0 && x+y>z && x+z>y && y+z>x)
+            {
+                s1 = x;
+                s2 = y;
+                s3 = z;
+                       
+            }
+            else
+            {
+                s1 = -1;
+                //Console.WriteLine("Not a valid Triangle");
+            }
+        }
+
+        public bool IsValid()
+        {
+            return s1 != -1;
+        }
 
         //TODO: 1. CREATE A SET FUNCTION THAT TAKES
         // X,Y,Z
         //ASSIGNS TO S1,S2,S3 ONLY IF X,Y,Z ARE VALID SIDES OF TRIANGLE
 
-
         //SHOULD RETURN -1 IF SIDES ARE NOT VALID
         public int Perimeter()
         {
-            return s1 + s2 + s3;
+            if (IsValid())
+                return s1 + s2 + s3;
+            else
+                return -1;
         }
 
         //SHOULD PRINT INVALID TRIANGLE IF SIDES ARE NOT VALID
         public void Draw()
         {
-            Console.WriteLine("Triangle<{0},{1},{2}> drawn", s1, s2, s3);
+            if(IsValid())
+                Console.WriteLine("Triangle<{0},{1},{2}> drawn", s1, s2, s3);
+            else
+                Console.WriteLine("Not a Valid Triangle");
         }
     }
 }
