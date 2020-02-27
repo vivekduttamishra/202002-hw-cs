0812bc6fd98c51af448888f60d9219c14512731f
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2020-02-27 11:17:32 +0530

    day#2 update#2

---
+++ csprojects/BankingSystem/BankConsoleApp01/Program.cs
@@ -0,0 +1,33 @@
+﻿using ConceptArchitect.Banking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankConsoleApp01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var icici = new Bank("ICICI", 12);
+
+            //creates an account. stores it internally. returns the account number
+            int a1 = icici.OpenAccount("Vivek Dutta Mishra", "p@ss", 20000);
+            int a2 = icici.OpenAccount("Prabhat Singh", "p@ss", 30000);
+
+            //bank (employee) can
+            icici.PrintAccountList(); //see a list of all accounts
+            icici.CreditInterest();   //credit interest to all accounts
+
+            //bank (customers) can
+            icici.Deposit(a1, 20000); //icici locates BankAccount object for a1 and add money
+            icici.Withdraw(a2, 5000, "p@ss"); //locate BankAccount for a1 and call its Withdraw
+            icici.Transfer(a1, 1000, "p@ss", a2); //transfer from a1 to a2 Rs 1000 using password
+
+
+
+        }
+    }
+}
---
+++ csprojects/BankingSystem/ConceptArchitect.Banking/Bank.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace ConceptArchitect.Banking
+{
+    public class Bank
+    {
+        private string name;
+        private double rate;
+
+        public Bank(string name, double rate)
+        {
+            this.name = name;
+            this.rate = rate;
+        }
+
+        public int OpenAccount(string customerName, string password, int amount)
+        {
+            //TODO: step 1. create BankAccount object
+
+            //TODO: step 2.  store the account object in Bank Object
+
+            //TODO: step 3.  return the account number
+            return 0;
+        }
+
+        public void PrintAccountList()
+        {
+            //TODO: print info about all accounts
+            
+        }
+
+        public void CreditInterest()
+        {
+            //TODO: credit interest to all accounts
+        }
+
+        public bool Deposit(int accountNumber, int amount)
+        {
+            //TODO: step 1. Locate the BankAccount object with given account number
+            BankAccount account = GetAccount(accountNumber);
+
+            //TODO: step 2. if there is a acccount with this account number deposit money
+            //if (/* your condition*/)
+            //    return account.Deposit(amount);
+            //else //no such account
+            //    return false;
+
+            return false;
+        }
+
+        public bool Transfer(int source, int amount, string password, int target)
+        {
+            //step1 Locate source account
+            BankAccount s = GetAccount(source);
+            BankAccount t = GetAccount(target);
+
+            //step2. check if both account exits. return false otherwise
+            //if (/*condition*/)
+            //    return false;
+
+            if (s.Withdraw(amount, password))
+                return t.Deposit(amount);
+            else
+                return false;
+        }
+
+        public bool Withdraw(int accountNumber  , double amount, string password)
+        {
+            //TODO: step 1. Locate the BankAccount object with given account number
+            BankAccount account = GetAccount(accountNumber);
+
+            //TODO: step 2. if there is a acccount with this account number deposit money
+            //if (/* your condition*/)
+            //    return account.Withdraw(amount);
+            //else //no such account
+            //    return false;
+
+            return false;
+        }
+
+        private BankAccount GetAccount(int accountNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
